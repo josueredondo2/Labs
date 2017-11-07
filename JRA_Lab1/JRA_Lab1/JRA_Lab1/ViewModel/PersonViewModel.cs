3f6859d68517a9fcadf99df51bde94314097f973
Josue Redondo <josue.redondo2@gmail.com>
2017-11-07 12:02:20 -0600

    Tarea 2
    Filtro y eliminacion de listview

--- JRA_Lab1/JRA_Lab1/JRA_Lab1/ViewModel/PersonViewModel.cs
+++ JRA_Lab1/JRA_Lab1/JRA_Lab1/ViewModel/PersonViewModel.cs
@@ -15,13 +15,15 @@ namespace JRA_Lab1.ViewModel
         #region Instances
         private ObservableCollection<PersonModel> _lstPersonList = new ObservableCollection<PersonModel>();
 
+        private ObservableCollection<PersonModel> _lstPersonListOriginal = new ObservableCollection<PersonModel>();
+
         public ObservableCollection<PersonModel> lstPersonList
         {
 
             get
             {
-                //Tarea
-                return (ObservableCollection<PersonModel>)_lstPersonList.Where(f => f.Nombre == _Filtro).ToList().AsEnumerable();
+                   
+                return _lstPersonList;
             }
 
             set
@@ -31,11 +33,12 @@ namespace JRA_Lab1.ViewModel
             }
         }
 
-        public string _NuevoIngreso;
+        private string _NuevoIngreso;
         public string NuevoIngreso
         {
             get
             {
+                
                 return _NuevoIngreso;
             }
             set
@@ -46,7 +49,39 @@ namespace JRA_Lab1.ViewModel
             }
         }
 
-        public string _Filtro;
+        private string _NuevoApellido;
+        public string NuevoApellido
+        {
+            get
+            {
+
+                return _NuevoApellido;
+            }
+            set
+            {
+                _NuevoApellido = value;
+                OnPropertyChanged("NuevoApellido");
+
+            }
+        }
+
+        private string _Descripcion;
+        public string NuevoDescripcion
+        {
+            get
+            {
+                return _Descripcion;
+            }
+            set
+            {
+                _Descripcion = value;
+                OnPropertyChanged("NuevoDescripcion");
+
+            }
+        }
+
+
+        private string _Filtro;
         public string Filtro
         {
             get
@@ -56,39 +91,97 @@ namespace JRA_Lab1.ViewModel
             set
             {
                 _Filtro = value;
+
+                if (_lstPersonListOriginal.Count > 0)
+                {
+                    if (!String.IsNullOrEmpty(_Filtro))
+                        lstPersonList = new ObservableCollection<PersonModel>(_lstPersonListOriginal.Where(a => a.NombreCompleto.ToLower().Contains(_Filtro.ToLower())));
+                    else
+                        lstPersonList = _lstPersonListOriginal;
+                }
+
                 OnPropertyChanged("Filtro");
 
             }
         }
+
+        private PersonModel _Persona;
+        public PersonModel Persona
+        {
+            get
+            {
+                return _Persona;
+            }
+            set
+            {
+                _Persona = value;
+                OnPropertyChanged("Persona");
+            }
+        }
+
         //interfaz de comandos
         public ICommand AgregarPersonaCommand { get; set; }
-
-        public ICommand BuscarPersonaCommand { get; set; }
+        public ICommand BorrarPersonaCommand { get; set; }
         #endregion
+
+        #region Metodos
 
         private void AgregarPersona()
         {
 
-            lstPersonList.Add(new PersonModel
+            _lstPersonListOriginal.Add(new PersonModel
             {
                 Nombre = NuevoIngreso,
-                Apellido = "Araya",
-                Descripcion = "Descripcion"
+                Apellido = NuevoApellido,
+                Descripcion = NuevoDescripcion
             });
+
+            lstPersonList = _lstPersonListOriginal;
+            NuevoIngreso = string.Empty;
+            NuevoApellido = string.Empty;
+            NuevoDescripcion = string.Empty;
         }
 
-        private void BuscarPersona()
+        private void BorrarPersona()
         {
-            
+            if (Persona != null)
+            {
+                _lstPersonListOriginal.Remove(Persona);
+                lstPersonList = _lstPersonListOriginal;
+            }
         }
+        #endregion
 
+        #region Contructor
         public PersonViewModel()
         {
             AgregarPersonaCommand = new Command(AgregarPersona);
-            BuscarPersonaCommand = new Command(BuscarPersona);
-        }
+            BorrarPersonaCommand = new Command(BorrarPersona);
 
-        
+            _lstPersonListOriginal.Add(new PersonModel
+            {
+                Nombre = "Josue",
+                Apellido = "Redondo",
+                Descripcion = "Desarrollador"
+            });
+
+            _lstPersonListOriginal.Add(new PersonModel
+            {
+                Nombre = "Cristiano",
+                Apellido = "Ronaldo",
+                Descripcion = "Real Madrid"
+            });
+
+            _lstPersonListOriginal.Add(new PersonModel
+            {
+                Nombre = "Leonel",
+                Apellido = "Messi",
+                Descripcion = "Barcelona"
+            });
+
+            lstPersonList = _lstPersonListOriginal;
+        }
+        #endregion
 
         #region Notified Interface
         //Un hilo que nos maneja entre las clases ciertos eventos(Handler)
